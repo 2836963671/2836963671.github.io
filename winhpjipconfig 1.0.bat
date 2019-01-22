943b99f30b6a42c942eaacdc67a068354cf4dcf8
hpj <45791705+2836963671@users.noreply.github.com>
2019-01-22 11:22:39 +0800

    Update winhpjipconfig 1.0.bat

--- winhpjipconfig 1.0.bat
+++ winhpjipconfig 1.0.bat
@@ -1,14 +1,11 @@
-@ echo off
+@ echo off    
 
 
-echo 你想知道自己的ip吗？你可以在此APP上知道，这不需要网络哦！
+echo 你想知道自己的ip吗？你可以在此APP上知道，这不需要网络哦！  pause  
 
 
-pause
-
-
-ipconfig
-echo 再按一次就是关掉此APP，请确保你的ip哟
+ipconfig     
+echo 再按一次就是关掉此APP，请确保你的ip哟  
 
 
 pause
